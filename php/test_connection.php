aadeac7199672b679f0ca4420ab57cb25ccb613b
larry487 <larray487@gmail.com>
2024-12-12 00:00:31 +0800

    2

--- php/test_connection.php
+++ php/test_connection.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "login");
+$mysqli = new mysqli("localhost", "root", "", "login_table_user");
 
 if ($mysqli->connect_error) {
     die("連接失敗: " . $mysqli->connect_error);
